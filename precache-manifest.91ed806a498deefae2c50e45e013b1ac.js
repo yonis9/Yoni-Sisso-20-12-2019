a108a2ffeb045adb989ed5c477e8f1a70fa3e394
Yoni Sisso <yonis9@walla.com>
2019-12-24 13:55:31 +0200

    Updates

---
+++ precache-manifest.91ed806a498deefae2c50e45e013b1ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0defd58b6b9f9a0cfeae675d3b67a5f7",
+    "url": "/Yoni-Sisso-20-12-2019/index.html"
+  },
+  {
+    "revision": "d0ab789850ad270cdeb8",
+    "url": "/Yoni-Sisso-20-12-2019/static/css/main.70adc9a9.chunk.css"
+  },
+  {
+    "revision": "c5364003ed26dcc5d7d2",
+    "url": "/Yoni-Sisso-20-12-2019/static/js/2.ec138101.chunk.js"
+  },
+  {
+    "revision": "b9bc2fc7725df2ce4b7409c91212bc51",
+    "url": "/Yoni-Sisso-20-12-2019/static/js/2.ec138101.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d0ab789850ad270cdeb8",
+    "url": "/Yoni-Sisso-20-12-2019/static/js/main.a5839afa.chunk.js"
+  },
+  {
+    "revision": "cff0fb05adc24dd528d0",
+    "url": "/Yoni-Sisso-20-12-2019/static/js/runtime-main.ff71ef8f.js"
+  }
+]);
