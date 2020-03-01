98eab76c47469fdb8b60ec4323d96d274ae7d8c1
Yoni Sisso <yonis9@walla.com>
2020-03-01 17:03:40 +0200

    made logger to be shown only in development

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,9 +8,12 @@ import thunkMiddleware from 'redux-thunk';
 import persistedReducer from './root-reducer';
 
 const middlewares = [
-    logger,
     thunkMiddleware
 ]
+
+if (process.env.NODE_ENV === 'development') {
+    middlewares.push(logger)
+}
 
 export const store = createStore(persistedReducer, applyMiddleware(...middlewares));
 
