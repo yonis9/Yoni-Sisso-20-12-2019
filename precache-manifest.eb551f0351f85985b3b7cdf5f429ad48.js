4add28de431ae062f668622049bc0b88bc85957f
Yoni Sisso <yonis9@walla.com>
2020-03-01 17:04:52 +0200

    Updates

---
+++ precache-manifest.eb551f0351f85985b3b7cdf5f429ad48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4071aad7e515589bebe67a4bec1352b1",
+    "url": "/Yoni-Sisso-20-12-2019/index.html"
+  },
+  {
+    "revision": "b6e9881dfb1ebd53b1da",
+    "url": "/Yoni-Sisso-20-12-2019/static/css/main.c441acf8.chunk.css"
+  },
+  {
+    "revision": "f91ff02251741f9b5b88",
+    "url": "/Yoni-Sisso-20-12-2019/static/js/2.71f87491.chunk.js"
+  },
+  {
+    "revision": "b9bc2fc7725df2ce4b7409c91212bc51",
+    "url": "/Yoni-Sisso-20-12-2019/static/js/2.71f87491.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b6e9881dfb1ebd53b1da",
+    "url": "/Yoni-Sisso-20-12-2019/static/js/main.8894b917.chunk.js"
+  },
+  {
+    "revision": "cff0fb05adc24dd528d0",
+    "url": "/Yoni-Sisso-20-12-2019/static/js/runtime-main.ff71ef8f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Yoni-Sisso-20-12-2019/precache-manifest.a1c70998f592ec64d29d97f4feeb7129.js"
+  "/Yoni-Sisso-20-12-2019/precache-manifest.eb551f0351f85985b3b7cdf5f429ad48.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Yoni-Sisso-20-12-2019/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
